2338ccbcc9a47c973b46a3fc7f8d863c63bbc1cf
MadInc63 <33602072+MadInc63@users.noreply.github.com>
2017-11-16 16:13:20 +0400

    Update bars.py

--- bars.py
+++ bars.py
@@ -1,21 +1,36 @@
 import json
+import sys
+from math import sqrt
 
 
 def load_data(filepath):
-    pass
+    try:
+        with open(filepath, 'r') as json_file:
+            json_data = json.loads(json_file.read())
+#            get_biggest_bar(json_data)
+#            get_smallest_bar(json_data)
+            longitude = 37
+            latitude = 55
+            get_closest_bar(json_data, longitude, latitude)
+    except IOError:
+        print('No such file or directory')
+    except ValueError:
+        print('File is empty')
 
 
-def get_biggest_bar(data):
-    pass
+def get_biggest_bar(json_data):
+    print(max(json_data['features'], key=lambda count:count['properties']['Attributes']['SeatsCount']))
 
+def get_smallest_bar(json_data):
+    print(min(json_data['features'], key=lambda count:count['properties']['Attributes']['SeatsCount']))
 
-def get_smallest_bar(data):
-    pass
+def get_closest_bar(json_data, longitude, latitude):
 
-
-def get_closest_bar(data, longitude, latitude):
-    pass
-
+    for bars in json_data['features']:
+        bar_longitude = bars['geometry']['coordinates'][0]
+        bar_latitude = bars['geometry']['coordinates'][1]
+        print(sqrt((bar_longitude-longitude)**2 + (bar_latitude-latitude)**2))
 
 if __name__ == '__main__':
-    pass
+    load_data('bars.json')
+#    load_data(sys.argv[1])
