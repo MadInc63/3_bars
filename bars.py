480d182c0054de7f53386dbe9e81a8d624345d71
Igor Statkevich <MadInc63@gmail.com>
2017-11-22 17:33:48 +0400

    fix all errors

--- bars.py
+++ bars.py
@@ -32,9 +32,9 @@ def get_smallest_bar(json_data):
 
 
 def get_closest_bar(json_data, longitude, latitude):
-    return (min(json_data['features'], key=lambda bar: sqrt(
-                (float(longitude)-bar['geometry']['coordinates'][0])**2 +
-                (float(latitude)-bar['geometry']['coordinates'][1])**2)))
+    return min(json_data['features'], key=lambda bar: sqrt(
+               (float(longitude)-bar['geometry']['coordinates'][0])**2 +
+               (float(latitude)-bar['geometry']['coordinates'][1])**2))
 
 
 def print_bar_info(bar):
